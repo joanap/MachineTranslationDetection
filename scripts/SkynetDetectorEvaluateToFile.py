73cf57f58a691ccee7a152aab2952c8f0f742525
Bruno Henriques <brunoaphenriques@gmail.com>
2016-03-21 12:30:45 +0000

    Adds unbabel blind test solution

--- scripts/SkynetDetectorEvaluateToFile.py
+++ scripts/SkynetDetectorEvaluateToFile.py
@@ -1,22 +1,30 @@
 import sys
 from SkynetDetector.SkynetDetector import SkynetDetector
 from Classifiers.SVMClassifier import *
+from Features.CountLeastFrequentTrigrams import *
+from Features.ConcordanceFeature import *
+from Features.RepeatedWordsCategory import *
+from Features.StopWordsCounter import *
+from Features.Util.POSTagger import POSTagger
+
 
 if __name__ == "__main__":
-    input_file = "../data/to_evaluate.txt"
-    train_file = "../data/train_dataset.txt"
+    input_file = "./data/test_blind.txt"
+    train_file = "./data/train_dataset.txt"
 
     if len(sys.argv) == 2:
         input_file = sys.argv[1]
     elif len(sys.argv) == 3:
         input_file, train_file = sys.argv[1], sys.argv[2]
-    else:
-        print "Incorrect number of arguments"
-        sys.exit(1)
+
+    ngram_words = NGramProbability("./data/output.bigram", "./data/output.trigram")
+
+    tagger = POSTagger()
+    tagger.train()
 
     # best features
     classifier = SVMClassifier()
-    features = []
+    features = [ConcordanceFeature(tagger, 1), ConcordanceFeature(tagger, 2), CountMostFrequentTrigrams(tagger, 0.85, 'words', ngram_words), RepeatedWordsCategory(tagger, [u'a', u'c', u'd', u'i', u'p', u'r', u's', u'v']), StopWordsCounter()]
 
     # Evaluate the accuracy of the model
     a = SkynetDetector(classifier, features)
@@ -24,4 +32,3 @@ if __name__ == "__main__":
     a.evaluate_file(input_file)
 
     sys.exit(0)
-
